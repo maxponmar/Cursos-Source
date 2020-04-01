7ba48ff1a0b95ac51a8cca9ce5c489fe5499f34a
Maximiliano <maxponce.marquez@gmail.com>
2020-04-01 06:08:21 -0700

    Django course

---
+++ 03_WebDevelopment/05_PythonDjango/01_WeatherApp/djangoenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/max/Cursos-Source-/03_WebDevelopment/05_PythonDjango/01_WeatherApp/djangoenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
