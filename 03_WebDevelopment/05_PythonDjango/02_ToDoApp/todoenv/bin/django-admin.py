cb463a0553cd5813de1e66ff491c89e8bdf19172
Maximiliano <maxponce.marquez@gmail.com>
2020-04-01 18:30:20 -0700

    djando todo app

---
+++ 03_WebDevelopment/05_PythonDjango/02_ToDoApp/todoenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/max/Cursos-Source/03_WebDevelopment/05_PythonDjango/02_ToDoApp/todoenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
