fda679c325b96fa4445c7a4897b10666dc44e2cc
Maximiliano <maxponce.marquez@gmail.com>
2020-04-02 17:20:30 -0700

    Django Course removed to another repo

--- 03_WebDevelopment/05_PythonDjango/02_ToDoApp/todo_app/todo_list/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'home.html', {})
-
-def about(request):
-    my_name = "Maximiliano Ponce Marquez"
-    context = {'name': my_name}
-    return render(request, 'about.html', context)
--- 03_WebDevelopment/05_PythonDjango/02_ToDoApp/todoenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/max/Cursos-Source/03_WebDevelopment/05_PythonDjango/02_ToDoApp/todoenv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
