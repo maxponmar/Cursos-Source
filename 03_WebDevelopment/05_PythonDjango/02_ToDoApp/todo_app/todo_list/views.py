178642dce98d73dacfcd6203eb2895bfdaef9914
Maximiliano <maxponce.marquez@gmail.com>
2020-04-02 17:13:06 -0700

    sql migration

--- 03_WebDevelopment/05_PythonDjango/02_ToDoApp/todo_app/todo_list/views.py
+++ 03_WebDevelopment/05_PythonDjango/02_ToDoApp/todo_app/todo_list/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html', {})
+
+def about(request):
+    my_name = "Maximiliano Ponce Marquez"
+    context = {'name': my_name}
+    return render(request, 'about.html', context)
